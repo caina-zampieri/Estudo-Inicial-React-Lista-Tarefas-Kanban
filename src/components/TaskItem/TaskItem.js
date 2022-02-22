f5a9e50495290dc01ae181b9ce77453567150ba6
omaigodi <caina.zampieri@gmail.com>
2022-02-22 10:36:41 -0300

    Atualizações da aula

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -2,32 +2,57 @@ import React, { useState } from "react";
 import "./taskitem.css";
 import PropTypes from "prop-types";
 
-export default function TaskItem({ id, title, taskState }) {
+export default function TaskItem({
+  id,
+  title,
+  taskState,
+  onTaskUpdate,
+  onDeleteTask
+}) {
   const [isEditing, setIsEditing] = useState(false);
   const [editableTitle, setEditableTitle] = useState(title);
 
   const onTitleChange = (event) => {
     const newTitle = event.target.value;
     setEditableTitle(newTitle);
+    onTaskUpdate(id, newTitle, taskState);
   };
 
   const onKeyPress = (event) => {
     if (event.key === "Enter") {
       setIsEditing(false);
+      if (editableTitle.length === 0) {
+        onDeleteTask(id);
+      }
     }
+  };
+
+  const onTaskStateChange = (event) => {
+    onTaskUpdate(id, title, event.target.value);
   };
 
   if (isEditing) {
     return (
-      <input
-        type="text"
-        value={editableTitle}
-        onChange={onTitleChange}
-        onKeyPress={onKeyPress}
-      />
+      <div className="task-item">
+        <input
+          type="text"
+          value={editableTitle}
+          onChange={onTitleChange}
+          onKeyPress={onKeyPress}
+        />
+      </div>
     );
   } else {
-    return <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>;
+    return (
+      <div className="task-item">
+        <div onClick={(e) => setIsEditing(true)}>{editableTitle}</div>
+        <select onChange={onTaskStateChange} value={taskState}>
+          <option value="Pendente">Pendente</option>
+          <option value="Fazendo">Fazendo</option>
+          <option value="Completa">Completa</option>
+        </select>
+      </div>
+    );
   }
 }
 
--- src/components/TaskList/tasklist.js
+++ src/components/TaskList/tasklist.js
@@ -1,11 +1,19 @@
 import React from "react";
 import "./tasklist.css";
-import PropTypes, { string } from "prop-types";
+import PropTypes from "prop-types";
 import TaskItem from "../TaskItem/TaskItem";
+import plusIcon from "../../img/plus-icon.svg";
 
-export default function TaskList({ title, onAddTask, tasks }) {
+export default function TaskList({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask
+}) {
   const addTask = () => {
-    onAddTask("Nova Tarefa", "Pendente");
+    onAddTask("Nova Tarefa", taskState);
   };
 
   return (
@@ -19,11 +27,16 @@ export default function TaskList({ title, onAddTask, tasks }) {
               id={task.id}
               title={task.title}
               taskState={task.state}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
             />
           );
         })}
       </div>
-      <button onClick={addTask}>Adicionar Tarefa</button>
+      <button onClick={addTask} className="btn">
+        <img src={plusIcon} alt="plus" />
+        Adicionar Tarefa
+      </button>
     </div>
   );
 }
